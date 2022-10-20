adb304b28bb15baa06486298e83300c2987e6683
StefaniaSperani <thedarkcry@hotmail.it>
2022-10-20 14:48:53 +0200

    array team js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,53 @@
 'use strict';
+/*
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+MILESTONE 0:
+x Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+x Stampare su console le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede e stilarli a vostro gusto
+BONUS 3:
+Aggiungere attraverso un form un membro al team
+*/
+
+//creo l'array del team
+const team = [
+    //dove ogni membro del team sarà un OGGETTO
+    {
+        nome: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        nome: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        nome: 'Walter Gordon',
+        role: 'Office Manager',
+        image: "walter-gordon-office-manager.jpg"
+    },
+    {
+        nome: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        nome: 'Scott Estrada',
+        role: 'Developer',
+        image: "scott-estrada-developer.jpg"
+    },
+    {
+        nome: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: "barbara-ramos-graphic-designer.jpg"
+    }
+];
+console.log(team);
